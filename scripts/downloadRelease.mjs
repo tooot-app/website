95ecc8f357d8115fc0bede6efe0040f3a8aff42d
xmflsct <me@xmflsct.com>
2022-12-04 21:45:26 +0000

    Fix version issue

--- scripts/downloadRelease.mjs
+++ scripts/downloadRelease.mjs
@@ -1,23 +1,18 @@
-import axios from 'axios'
 import fs from 'fs'
 
-const main = () => {
-  axios
-    .get('https://api.github.com/repos/tooot-app/app/releases', {
-      headers: {
-        Accept: 'application/vnd.github.v3+json',
-        Authorization: `token ${process.env.GITHUB_TOKEN}`
-      }
-    })
-    .then(async ({ data }) => {
-      const latest = data.find(d => !d.prerelease)
+fetch('https://api.github.com/repos/tooot-app/app/releases', {
+  headers: {
+    Accept: 'application/vnd.github+json',
+    Authorization: `token ${process.env.GITHUB_TOKEN}`
+  }
+})
+  .then(async res => {
+    const data = await res.json()
+    const latest = data.find(d => !d.prerelease)
 
-      fs.writeFileSync(
-        './public/version.json',
-        JSON.stringify({ latest: latest.name.replace('v', '') })
-      )
-    })
-    .catch(err => console.log(err))
-}
-
-main()
+    fs.writeFileSync(
+      './public/version.json',
+      JSON.stringify({ latest: latest.name.replace('v', '') })
+    )
+  })
+  .catch(err => console.log(err))
